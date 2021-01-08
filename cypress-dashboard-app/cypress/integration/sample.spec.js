78a68c1113160735dabc03bdd2b4ed27ee960a33
MNikhil0210 <nikhilpavansai.machavaram@zemosolabs.com>
2021-01-08 17:09:52 +0530

    fix: trigger pipeline on push to every branch

--- cypress-dashboard-app/cypress/integration/sample.spec.js
+++ cypress-dashboard-app/cypress/integration/sample.spec.js
@@ -1,10 +1,10 @@
-describe("Add plates via Bulk upload CSV file", () => {
+describe("Sample text check", () => {
 
   before(() => {
     cy.visit("http://localhost:3000");
   });
 
-  it("Check if newly added plates are visible in inventory", () => {
+  it("Cypress dashboard test", () => {
     cy.get("#root > main > div.makeStyles-paper-1 > h1").should("have.text", "Sign in");
   });
 });
